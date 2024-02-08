ee7bec53d2d68ba8ca77ea4fea9195ed628ecb05
cmartinez11 <alex95svr@gmail.com>
2024-02-07 23:56:03 -0500

    Update test_main.py
    se hizo cambios en los comentarios ya que estaba mal

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,12 +1,12 @@
 from main import validar_longitud
 
 def test_longitud_valida():
-    #estamos validando que nuestra cadena "Hola" "Python" y "12345678"
+    #estamos validando que nuestra cadena "Hola" "Python" y "12345678" sean verdaderas, con solo la primera sentencia nos podemos dar cuenta que las demas estan bien
     assert validar_longitud("Hola") == True
     assert validar_longitud("Python") == True
     assert validar_longitud("12345678") == True
 
-#estamos validando que nuestra cadena "" "Hola, mundo" y "12345678"
+#estamos validando que nuestra cadena "" "Hola, mundo" y "1234567890" sean falsas, con solo la primera sentencia nos podemos dar cuenta que las demas estan bien
 def test_longitud_invalida():
     assert validar_longitud("") == False
     assert validar_longitud("Hola, mundo") == False
